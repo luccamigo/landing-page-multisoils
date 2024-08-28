458a8998ff9ce61f34878c7e91ba3cd10de725ab
Lucca Amigo <lucca.amigo@gmail.com>
2024-08-28 11:18:27 -0300

    primeira parte completa
    preciso ainda mexer na responsividade de "entrar" e "cadastre-se"

--- multisoils/src/components/Navbar.jsx
+++ multisoils/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Navbar.css';
+import image from "../assets/MultiSoils.png"
 
 const Navbar = () => {
     return (
         <header className="navbar">
             <nav className="navbar-container">
-                <div className="logo">MultiSoils</div>
+                <div className="logo"><img src={image} alt="logo Multisoils" /></div>
                 <div className="menu">
                     <button className="btn cadastre-se">Cadastre-se</button>
                     <button className="btn entrar">Entrar</button>
--- multisoils/src/components/heroSection.jsx
+++ multisoils/src/components/heroSection.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
 import './heroSection.css';
+import image from '../assets/semeandoosolo2iStock.jpg'
 // Substitua pelo caminho correto da imagem
 
 const heroSection = () => {
     return (
         <main className="main-section">
             <div className="main-image">
-                <img src="C:\PSW\landing-page-multisoils\multisoils\assets\semeandoosolo2iStock.jpg" alt="Plantando sementes" />
+                <img src={image} alt="Plantando sementes" />
             </div>
             <div className="main-text">
                 <h1>Lorem Ipsum</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eu vehicula justo...</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Ut eu luctus dolor, at condimentum massa. Aliquam erat volutpat.
+                    Curabitur nec sapien tristique, sodales odio vitae, ornare eros.
+                    Nunc quis faucibus justo. Sed at enim a massa malesuada venenatis.
+                    Vivamus tempor ex a eros rutrum lacinia.
+                    Interdum et malesuada fames ac ante ipsum primis in faucibus.
+                    Aenean ac suscipit ante, a euismod odio. Donec et malesuada sem,
+                    sit amet dapibus est. Nulla rhoncus nisl vel mauris pulvinar, in finibus metus iaculis.
+                    Proin euismod, est non auctor fermentum, augue quam venenatis dolor, sit amet sollicitudin tortor metus at magna.
+                    Praesent faucibus ligula neque, volutpat sodales metus maximus nec.
+                    Donec eget risus nec elit aliquet convallis in eu libero.
+                    Cras massa orci, rutrum quis dignissim vel, tempus quis velit.</p>
+                <div className='centralizer'>
+                    <button className='btn acesseagr'>Acesse Agora!</button>
+                </div>
+
             </div>
         </main>
     );
